447506821d933148fc4704352062430ca74f0525
Giovanna Fernandes <pfernandesgiovanna07@gmail.com>
2026-06-04 01:05:21 -0300

    Global Solution POO

--- src/br/com/orbitalwatch/model/SistemaPropulsao.java
+++ src/br/com/orbitalwatch/model/SistemaPropulsao.java
@@ -1,3 +1,10 @@
+/**
+ * Classe abstrata que representa um sistema de propulsão genérico.
+ * 
+ * Serve como base para diferentes tipos de propulsão e demonstra
+ * herança, métodos concretos e métodos abstratos.
+ */
+
 package br.com.orbitalwatch.model;
 
 public abstract class SistemaPropulsao {
@@ -23,10 +30,24 @@ public abstract class SistemaPropulsao {
         System.out.println(this.nome + " desligado.");
     }
 
+/**
+ * Método abstrato que obriga cada tipo de propulsão a implementar
+ * sua própria forma de aceleração.
+ */
+    
     public abstract void acelerar(double percentual);
 
+/**
+ * Método abstrato que obriga cada tipo de propulsão a calcular
+ * o empuxo de acordo com sua própria regra.
+ */
+    
     public abstract double calcularEmpuxo();
 
+/**
+ * Valida se a potência informada está dentro do intervalo permitido de 0 a 100%.
+ */
+    
     protected boolean validarPotencia(double percentual) {
         if (percentual < 0 || percentual > 100) {
             System.out.println("Erro: potência deve estar entre 0 e 100%.");
