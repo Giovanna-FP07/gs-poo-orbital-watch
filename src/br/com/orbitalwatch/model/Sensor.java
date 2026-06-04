4c84af976fbfd58b1b925052a0365931fe8c377c
Giovanna Fernandes <pfernandesgiovanna07@gmail.com>
2026-06-04 00:58:03 -0300

    Global Solution POO

--- src/br/com/orbitalwatch/model/Sensor.java
+++ src/br/com/orbitalwatch/model/Sensor.java
@@ -1,3 +1,9 @@
+/**
+ * Interface que define o contrato obrigatório para todos os sensores.
+ * Toda classe que implementar Sensor deve possuir métodos de leitura,
+ * verificação de funcionamento, retorno de tipo e alerta.
+ */
+
 package br.com.orbitalwatch.model;
 
 public interface Sensor {
